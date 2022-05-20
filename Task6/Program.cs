666d454d62bd0cf1fe9d00429107ba4ea1b08a85
Kiryl Pashkin <sleembelbox@gmail.com>
2022-05-20 14:11:40 +0300

    modified task6

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,4 +1,6 @@
 ﻿// Показать кубы чисел, заканчивающихся на четную цифру
+// Это решение, если куб числа четное число
+/*  
 Console.Clear();
 Console.WriteLine("Введите число: ");
 int number = int.Parse(Console.ReadLine()?? "");
@@ -9,3 +11,12 @@ for (int i = 1; i <= number; i++)
     int cubeNumber = (int)Math.Pow(i,3);
     if(cubeNumber%10%2 == 0) Console.WriteLine(cubeNumber);
 }
+*/
+// Это решение. Если введеное число четное, то показать его куб
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()?? "");
+
+Console.WriteLine();
+if(number%2 == 0) Console.WriteLine($"Куб четного числа {number} равен {Math.Pow(number,3)}");
+else Console.WriteLine("Введенное число не четное.");
