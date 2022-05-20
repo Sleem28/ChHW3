04bf66e5bc8a05518c4426ce5be9b7f276adc44d
Kiryl Pashkin <sleembelbox@gmail.com>
2022-05-20 13:53:23 +0300

    add task6

---
+++ Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()?? "");
+
+Console.WriteLine();
+for (int i = 1; i <= number; i++)
+{
+    int cubeNumber = (int)Math.Pow(i,3);
+    if(cubeNumber%10%2 == 0) Console.WriteLine(cubeNumber);
+}
