27857be3f4e9c2e516d77803a2ee9589c376f292
Kiryl Pashkin <sleembelbox@gmail.com>
2022-05-20 11:36:09 +0300

    add task1

---
+++ Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+Console.Write("\n" + "Введите число, для построения таблицы квадратов: ");
+int number = int.Parse(Console.ReadLine()?? "");
+Console.WriteLine($"\n" + $"Таблица квадратов от 1 до {number}:\n");
+
+for (int i = 0; i <= number; i++)
+{
+    Console.WriteLine($"Квадрат числа {i} равен {i*i}");
+}
+
