c3907879bea16c87a0a3137be2aabbf3993fc580
Kiryl Pashkin <sleembelbox@gmail.com>
2022-05-20 13:29:32 +0300

    add task4

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+Console.Clear();
+Console.WriteLine("Введите число А: ");
+int number = int.Parse(Console.ReadLine()?? "");
+Console.WriteLine("\n" + "Введите число В: ");
+uint degree = uint.Parse(Console.ReadLine()?? "");
+
+int result = number;
+
+for (int i = 1; i < degree; i++)
+{
+    result *= number;
+}
+
+Console.WriteLine($"Число {number} в степени {degree} равно {result}");
