8d791021ebe1c8a1baa57dab23f9000fb36dc57c
Kiryl Pashkin <sleembelbox@gmail.com>
2022-05-20 13:41:03 +0300

    add task5

---
+++ Task5/Program.cs
@@ -0,0 +1,15 @@
+﻿//Подсчитать сумму цифр в числе
+// Все как вы любите. Никаких string.length  и т.д.
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()?? "");
+int sumDigits = 0;
+
+while(true)
+{
+    sumDigits += number%10;
+    if(number < 10) break;
+    number /= 10;
+}
+
+Console.WriteLine("Сумма цифр введенного числа равна " + sumDigits);
