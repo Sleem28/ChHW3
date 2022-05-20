0d51fa48480b003fa8d26cc0062832e23379bab0
Kiryl Pashkin <sleembelbox@gmail.com>
2022-05-20 13:10:24 +0300

    add task3

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти сумму чисел от 1 до А
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()?? "");
+int sum = 0;
+
+for (int i = 1; i <= number; i++)
+{
+    sum += i;
+}
+
+Console.WriteLine($"Сумма чисел от 1 до {number} равна {sum}.\n");
